2f400f9eb05a7aa45a92503e927ef4ee9af51748
realllllyx18 <71406802+realllllyx18@users.noreply.github.com>
2020-09-29 18:07:35 -0700

    Update task3.py

--- task3.py
+++ task3.py
@@ -3,19 +3,22 @@
 Ask the user to enter in a number.
 Have them repeat this until the number is an even integer.
 (2 marks)
-
-
 inputs:
     float number
-
 outputs:
     That is an even integer
     That is not an even integer
-
 Examples:
 Enter number:1.3
 That is not an even integer
 Enter number:4
 That is an even integer
-
 """
+
+number=input("Enter a number")
+x=float(number)
+
+if x%1==0 and x%2==0:
+    print("That is an even integer")
+else:
+    print("That is not an even integer")
