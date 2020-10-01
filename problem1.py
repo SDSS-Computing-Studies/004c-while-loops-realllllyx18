5f5ff8f55644a5ad3d8db691ec628bce60cf317c
realllllyx18 <71406802+realllllyx18@users.noreply.github.com>
2020-10-01 11:04:40 -0700

    Update problem1.py

--- problem1.py
+++ problem1.py
@@ -26,9 +26,9 @@ while username != "admin" or password != "12345":
     username = (input("Enter a username:")).strip()
     password = (input("Enter a password:")).strip()
     count=count+1
-    if count == 3:
-        break
     if username != "admin" or password != "12345":
         print("Access denied")
+    if count == 3:
+        break
     if username == "admin" and password == "12345":
         print("Access granted")
